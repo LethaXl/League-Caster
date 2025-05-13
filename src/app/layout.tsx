a8b84d99c0ff7bd66343d66f10ffd90d739b4ab3
LethaXl <aseef217@gmail.com>
2025-05-13 17:15:40 +0300

    Layout changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-6482912478758155" />
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6482912478758155"
